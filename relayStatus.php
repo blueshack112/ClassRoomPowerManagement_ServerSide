a4014b8ddd7d805eb922216ebaf9fa0b8a917cfe
blueshack112 <ahmed.hassan.112.ha@gmail.com>
2019-12-26 19:48:09 +0500

    added relayStatus.php to get open relays in a room

--- relayStatus.php
+++ relayStatus.php
@@ -15,8 +15,52 @@ class RelayStatusResponse {
     }     
 }
 
+//Connection properties 
+$servername = "localhost";
+$username = "root";
+$password = "admin";
+
+//Create connection
+$conn = new mysqli($servername, $username, $password);
+
+//Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $roomID = $_POST['roomID'];
 
+$getRelays = "SELECT relay_used FROM db_classroom_management.tbl_room_status WHERE room_id = $roomID";
+$getRelaysResult = mysqli_query($conn, $getRelays);
+$relayNo = mysqli_num_rows($getRelaysResult);
 
+// If room status is active...
+if ($relayNo > 0) {
+    $response = new RelayStatusResponse();
+    $response->successful = true;
+    $response->roomActive = true;
+
+    $row = mysqli_fetch_assoc($getRelaysResult);
+    $relayString = $row['relay_used'];
+
+    while (true) {
+        $tempRelay = substr($relayString, 0, strpos($relayString, ","));
+        // Break the loop if the string no longer has a ','
+        if (!$tempRelay) {
+            $response->relaysOn[] = $relayString;
+            break;
+        }
+        $relayString = substr($relayString, strpos($relayString, ",")+1);
+
+        // Add relay to the array
+        $response->relaysOn[] = $tempRelay;
+    }
+    echo (json_encode($response));
+} else {
+    $response = new RelayStatusResponse();
+    $response->successful = true;
+    $response->roomActive = false;
+    echo (json_encode($response));
+}
 
 ?>
